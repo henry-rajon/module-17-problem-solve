24848b8b059af984bea9c3ab7ecfd8f07fa6ae7b
Raj_Dash <rajdash3472@gmail.com>
2022-08-01 21:56:24 +0600

    Problem-1 Solved

---
+++ problem-solve.js
@@ -0,0 +1,20 @@
+// Problem-1
+
+var fruits = ['Apple', 'Banana', 'Orange'];
+
+// Find the index
+var index = fruits.indexOf('Banana');
+// console.log(index);
+
+// Replace
+
+fruits[1] = 'Mango';
+// console.log(fruits);
+
+// Remove 'Orange'
+fruits.pop()
+// console.log(fruits);
+
+// Add 'Watermelon'
+fruits.push('Watermelon');
+// console.log(fruits);
