94ff211eb833f4404758cb6e6be200634fa5fcd3
Raj_Dash <rajdash3472@gmail.com>
2022-08-01 22:27:46 +0600

    Problem-2 Solved

--- problem-solve.js
+++ problem-solve.js
@@ -18,3 +18,24 @@ fruits.pop()
 // Add 'Watermelon'
 fruits.push('Watermelon');
 // console.log(fruits);
+
+
+// Problem-2
+
+var markes = 30;
+
+if (markes >= 80) {
+    console.log('Grad A');
+}
+else if (markes >= 60) {
+    console.log('Grad B');
+}
+else if (markes >= 50) {
+    console.log('Grad C');
+}
+else if (markes >= 40) {
+    console.log('Grad D')
+}
+else if (markes <= 39) {
+    console.log('Grad F')
+}
